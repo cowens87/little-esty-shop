fc58148cff1df7c0cb4266c394c8e3d608681c52
Cydnee Owens <cks410@yahoo.com>
2021-01-14 21:52:44 -0700

    add feature testing

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'As a merchant', type: :feature do
   describe 'When I visit my bulk discounts index page' do
     before(:each) do
-              @sally       = Customer.create!(first_name: 'Sally', last_name: 'Smith')
+        @sally       = Customer.create!(first_name: 'Sally', last_name: 'Smith')
         @joel        = Customer.create!(first_name: 'Joel', last_name: 'Hansen')
         @john        = Customer.create!(first_name: 'John', last_name: 'Hansen')
         @travolta    = Customer.create!(first_name: 'Travolta', last_name: 'Hansen')
@@ -37,29 +37,29 @@ RSpec.describe 'As a merchant', type: :feature do
         @tx10        = Transaction.create!(result: "success", credit_card_number: 010001005523, credit_card_expiration_date: 20220101, invoice_id: @invoice10.id,)
         @tx11        = Transaction.create!(result: "success", credit_card_number: 0100010055, credit_card_expiration_date: 20220101, invoice_id: @invoice11.id,)
         @tx12        = Transaction.create!(result: "failure", credit_card_number: 0100010055, credit_card_expiration_date: 20220101, invoice_id: @invoice14.id,)
-        # @discount_10 = Discount.create!(discount_percentage: 10, quantity_threshold: 5, merchant_id: @max.id)
-        # @discount_15 = Discount.create!(discount_percentage: 15, quantity_threshold: 10, merchant_id: @max.id)
-        # @discount_20 = Discount.create!(discount_percentage: 20, quantity_threshold: 20, merchant_id: @max.id)
+        @discount_10 = Discount.create!(discount_percentage: 10, quantity_threshold: 5, merchant_id: @amazon.id)
+        @discount_15 = Discount.create!(discount_percentage: 15, quantity_threshold: 10, merchant_id: @amazon.id)
+        @discount_20 = Discount.create!(discount_percentage: 20, quantity_threshold: 20, merchant_id: @amazon.id)
     end
+    # Individual Project Stories:
+    describe 'Merchant Bulk Discount Create' do
+      it 'I see a link to create a new discount' do
+        visit merchant_discounts_path(@max.id)
 
-    # describe 'Merchant Bulk Discount Create' do
-    #   it 'I see a link to create a new discount' do
-    #     visit merchant_discounts_path(@max.id)
+        expect(page).to have_link('Create a New Discount')
 
-    #     expect(page).to have_link('Create a New Discount')
+        click_link('Create a New Discount')
 
-    #     click_link('Create a New Discount')
+        expect(current_path).to eq(new_merchant_discount_path)
 
-    #     expect(current_path).to eq(new_merchant_discount_path)
+        fill_in 'Discount Percentage:', with: 25
+        fill_in 'Quantity Threshold:', with: 50
+        click_on 'Submit'
 
-    #     fill_in 'Discount Percentage:', with: 25
-    #     fill_in 'Quantity Threshold:', with: 50
-    #     click_on 'Submit'
-
-    #     expect(current_path).to eq(merchant_discounts_path(@max.id))
-    #     expect(page).to have_content("Discount Percentage: 25, Quantity: 50")
-    #   end
-    # end
+        expect(current_path).to eq(merchant_discounts_path(@max.id))
+        expect(page).to have_content("Discount Percentage: 25, Quantity: 50")
+      end
+    end
 
     # describe 'Merchant Bulk Discount Delete' do
     #   it 'Next to each bulk discount I see a link to delete it' do
